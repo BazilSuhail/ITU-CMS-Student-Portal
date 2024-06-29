709c1927593515f7893bbf204856a5ce91ad144a
BazilSuhail <bazil1854@gmail.com>
2024-06-29 15:32:23 +0500

    Fixed Navbar !

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { auth, fs } from "../Config/Config";
 import { PiStudentDuotone } from "react-icons/pi";
-
 import { TbSquareRoundedPercentage } from "react-icons/tb";
 import { LuBookOpen } from "react-icons/lu";
 import { IoMdBookmarks } from "react-icons/io";
@@ -11,41 +10,47 @@ import { PiHandWithdrawLight } from "react-icons/pi";
 import { ImCross } from "react-icons/im";
 import { RiMenu3Line } from "react-icons/ri";
 
-
-
-import logo from "./itu.png"
-import profile from "./Profile1.png"
+import logo from "./itu.png";
+import profile from "./Profile1.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [userName, setUserName] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const [isXsx, setIsXsx] = useState(window.innerWidth >= 1600); // Adjust according to your custom 'xsx' size
+  const [isXsx, setIsXsx] = useState(window.innerWidth >= 1600);
 
   useEffect(() => {
     const fetchUserName = async () => {
       const user = auth.currentUser;
       if (user) {
-        const userDoc = await fs.collection("students").doc(user.uid).get();
-        if (userDoc.exists) {
-          setUserName(userDoc.data().name); // Assuming 'name' is the field in the document
+        try {
+          const userDoc = await fs.collection("students").doc(user.uid).get();
+          if (userDoc.exists) {
+            setUserName(userDoc.data().name);
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
         }
       }
     };
     fetchUserName();
 
     const handleResize = () => {
-      setIsXsx(window.innerWidth >= 1600); // Adjust according to your custom 'xsx' size
+      setIsXsx(window.innerWidth >= 1600);
     };
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const handleLogout = () => {
-    auth.signOut();
-    navigate("/signin");
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      navigate("/signin");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
   };
 
   const toggleNavbar = () => {
@@ -56,13 +61,13 @@ const Navbar = () => {
     <nav>
       {/* Navbar for larger screens ---- Show this div on all screens larger than small */}
       <div className="mt-[6px] fixed hidden xsx:flex xsx:flex-col xsx:justify-between shadow-xl rounded-2xl m-[10px] xsx:items-center w-[260px] h-[98vh] bg-custom-blue p-[10px]">
-        <div className="w-[100%] flex justify-evenly">
+        <div className="mt-[8px] w-[100%] flex">
 
-          <img src={profile} alt="" className="w-[70px] h-[70px] " />
+          <img src={profile} alt="" className="ml-[25px] w-[70px] h-[70px] " />
 
           <div className="ml-[20px]">
-            <div className="text-white mt-[10px] text-sm text-center">Welcome,</div>
-            <p className="text-white text-xsx text-center">{userName} !!</p>
+            <div className="text-white ml-[0px] mt-[10px] text-md text-center">Welcome,</div>
+            <p className="text-white  text-3xl text-center">{userName} !</p>
           </div>
 
         </div>
@@ -119,7 +124,90 @@ const Navbar = () => {
         </div>
         <button onClick={handleLogout} className="text-2xl mb-[10px] p-[8px] font-bold w-[90%] text-white bg-red-800 rounded-lg ">Logout</button>
 
-       
+      </div>
+
+      {/* Navbar for small screens */}
+      {location.pathname !== '/signin' && (
+        <div className="xsx:hidden bg-custom-blue w-full h-[80px] z-35 flex items-center" onClick={toggleNavbar}>
+          <img src={logo} alt="" className="w-[50px] ml-[14px] h-[50px] " />
+          <button className="text-white ml-auto mr-[18px]">{isOpen ? <p></p> : <p><RiMenu3Line size={25} /></p>}</button>
+        </div>
+      )}
+      
+      {/* Conditionally render the first div for smaller screens */}
+      {!isXsx && isOpen && (
+
+        <div className={`fixed z-40 flex flex-col justify-between items-center top-0 left-0 w-[270px] h-[90vh] bg-custom-blue p-[10px] shadow-xl rounded-2xl m-[10px] transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+
+          <button className="bg-red-500 text-white rounded-full p-[8px] ml-auto mb-[-85px]" onClick={toggleNavbar}><ImCross size={12} /></button>
+
+          <div className="w-[100%] flex">
+
+            <img src={profile} alt="" className="ml-[15px] w-[70px] h-[70px] " />
+
+            <div className="ml-[20px]">
+              <div className="text-white ml-[-15px] mt-[13px] mb-[-5px] text-sm text-center">Welcome,</div>
+              <div className="text-white text-4xl ml-[-5px] text-center">{userName} !!</div>
+            </div>
+
+          </div>
+
+          <div className="flex text-white flex-col w-[98%] mt-[-25px]">
+
+            <NavLink
+              to="/student-profile"
+              className={({ isActive }) =>
+                `flex items-center p-2 rounded-md text-md ${isActive ? 'bg-custom-back-grey text-white font-bold my-[6px]' : 'my-[6px] hover:bg-custom-back-grey hover:rounded-2xl hover:font-bold text-white'}`
+              }
+            >
+              <PiStudentDuotone className="text-custom-sz mr-2" />
+              <p className="mt-0">Dashboard</p>
+            </NavLink>
+
+
+            <NavLink
+              to="/completedcourses"
+              className={({ isActive }) =>
+                `flex items-center p-2 rounded-md text-md ${isActive ? 'bg-custom-back-grey text-white font-bold my-[6px]' : 'my-[6px] hover:bg-custom-back-grey hover:rounded-2xl hover:font-bold text-white'}`
+              }
+            >
+              <LuBookOpen className="text-custom-sz mr-2" />
+              <p className="">Academia</p>
+            </NavLink>
+            <NavLink
+              to="/attendance"
+              className={({ isActive }) =>
+                `flex items-center p-2 rounded-md text-md ${isActive ? 'bg-custom-back-grey text-white font-bold my-[6px]' : 'my-[6px] hover:bg-custom-back-grey hover:rounded-2xl hover:font-bold text-white'}`
+              }
+            >
+              <TbSquareRoundedPercentage className="text-custom-sz mr-2" />
+              <p>Attendance</p>
+            </NavLink>
+
+            <NavLink
+              to="/viewmarks"
+              className={({ isActive }) =>
+                `flex items-center p-2 rounded-md text-md ${isActive ? 'bg-custom-back-grey text-white font-bold my-[6px]' : 'my-[6px] hover:bg-custom-back-grey hover:rounded-2xl hover:font-bold text-white'}`
+              }>
+              <IoMdBookmarks className="text-custom-sz mr-2" />
+              <p>Marks</p>
+            </NavLink>
+
+            <div className="w-[95%] rounded-lg h-[2px] bg-custom-back-grey mx-auto my-[5px]"></div>
+
+
+            <div className="ml-[6px] text-lg mb-[14px]">Checkout</div>
+
+            <NavLink to="/enrollcourses" className="ml-[15px] mt-[6px] hover:bg-custom-back-grey hover:rounded-xl hover:font-bold  text-sm  text-white rounded-md p-[8px] w-[90%] flex flex-row "> <MdScreenRotation className="mb-[5px] mr-[4px] text-custom-size" /><p className="mt-[2px]">Enroll Courses</p></NavLink>
+            <NavLink to="/withdrawcourses" className="ml-[15px] mt-[6px] hover:bg-custom-back-grey hover:rounded-xl hover:font-bold  text-sm  text-white rounded-md p-[8px] w-[90%] flex flex-row "> <PiHandWithdrawLight className="mb-[5px] mr-[4px] text-custom-size" /><p className="mt-[2px]">Withdraw Courses</p></NavLink>
+
+
+          </div>
+
+          <button onClick={handleLogout} className="text-2xl mb-[10px] p-[8px] font-bold w-[90%] text-white bg-red-800 rounded-lg ">Logout</button>
+
+        </div>
+      )}
     </nav>
   );
 };
